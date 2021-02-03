5ef447826298c361cb3c684659ab645bc8bc194d
Xie Xie <DSAD2015@163.com>
2021-02-03 16:17:14 +0800

    Create billionsort_set.cpp

---
+++ billionsort/billionsort_set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <random>
+#include <chrono>
+
+int main()
+{
+  // 计时器start_t, end_t.
+
+  // 内存分配计时开始.
+  auto start_t = std::chrono::steady_clock::now();
+  std::vector<double> V(1000000000);  // 10亿个数, 需要7.5GB内存.
+  // 内存分配计时结束并输出时间.
+  auto end_t = std::chrono::steady_clock::now();
+  std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
+            << " minutes" << std::endl;
+
+  // 数据赋值计时开始.
+  start_t = std::chrono::steady_clock::now();
+  // 利用随机数生成器生成0.0到1.0之间的实数.
+  std::default_random_engine generator(time(NULL));
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  for (size_t i = 0; i < V.size(); ++i)
+    V[i] = distribution(generator);
+  // 数据赋值计时结束并输出时间.
+  end_t = std::chrono::steady_clock::now();
+  std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
+            << " minutes" << std::endl;
+
+  // 排序计时开始.
+  start_t = std::chrono::steady_clock::now();
+  // 插入到集合中相当于对10亿个随机数排序, 集合会耗费45GB左右.
+  std::multiset<double> S;
+  for (auto iter = V.cbegin(); iter != V.cend(); ++iter)
+    S.insert(*iter);
+  // 排序计时结束并输出时间.
+  end_t = std::chrono::steady_clock::now();
+  std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
+            << " minutes" << std::endl;
+
+  return 0;
+}
