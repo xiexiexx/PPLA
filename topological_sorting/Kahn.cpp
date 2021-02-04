dcd2af7ecb915236345f63b3b5b6aa4579725e3d
Xie Xie <DSAD2015@163.com>
2021-02-04 11:42:59 +0800

    Create Kahn.cpp

---
+++ topological_sorting/Kahn.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::vector;
+using std::queue;
+using std::cout;
+
+using graph = vector<vector<size_t>>;
+
+void Kahn(const graph& G)
+{
+  vector<size_t> in_degree(G.size(), 0);
+  for (size_t u = 0; u < G.size(); ++u)
+    for (const auto& v : G[u])
+      ++in_degree[v];
+  queue<size_t> Q;
+  for (size_t v = 0; v < in_degree.size(); ++v)
+    if (in_degree[v] == 0)
+      Q.push(v);
+  while (!Q.empty())
+  {
+    size_t u = Q.front();
+    Q.pop();
+    cout << u << ' ';
+    for (const auto& v : G[u])
+      if (--in_degree[v] == 0)
+        Q.push(v);
+  }
+}
+
+int main()
+{
+  graph G {{1, 2}, {}, {4}, {2}, {}, {1}};
+  Kahn(G);
+  return 0;
+}
