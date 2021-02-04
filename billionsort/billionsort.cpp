ce63786bc52eeede22d0dbfe90198c9449379cfe
Xie Xie <DSAD2015@163.com>
2021-02-04 23:44:57 +0800

    Update billionsort.cpp

--- billionsort/billionsort.cpp
+++ billionsort/billionsort.cpp
@@ -4,38 +4,41 @@
 #include <random>
 #include <chrono>
 
-int main()
+// 打印耗时情况.
+inline void print_duration(std::chrono::steady_clock::time_point start_t,
+                           std::chrono::steady_clock::time_point end_t)
 {
-  // 计时器start_t, end_t.
-
-  // 内存分配计时开始.
-  auto start_t = std::chrono::steady_clock::now();
-  std::vector<double> V(1000000000);  // 10亿个数, 需要7.5GB内存.
-  // 内存分配计时结束并输出时间.
-  auto end_t = std::chrono::steady_clock::now();
   std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
             << " minutes" << std::endl;
+}
 
-  // 数据赋值计时开始.
+int main()
+{
+  // 请注意: 10亿个数, 需要7.5GB内存.
+  // 计时器start_t, end_t.
+
+  // 内存分配.
+  auto start_t = std::chrono::steady_clock::now();
+  std::vector<double> V(1000000000);
+  auto end_t = std::chrono::steady_clock::now();
+  print_duration(start_t, end_t);
+
+  // 数据赋值.
   start_t = std::chrono::steady_clock::now();
   // 利用随机数生成器生成0.0到1.0之间的实数.
   std::default_random_engine generator(time(NULL));
   std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  for (size_t i = 0; i < V.size(); ++i)
-    V[i] = distribution(generator);
-  // 数据赋值计时结束并输出时间.
+  for (auto& x : V)
+    x = distribution(generator);
   end_t = std::chrono::steady_clock::now();
-  std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
-            << " minutes" << std::endl;
+  print_duration(start_t, end_t);
 
-  // 排序计时开始.
+  // 串行排序.
   start_t = std::chrono::steady_clock::now();
   // 对10亿个随机数排序, 如果用数组时间也没什么太大差别.
   std::sort(V.begin(), V.end());
-  // 排序计时结束并输出时间.
   end_t = std::chrono::steady_clock::now();
-  std::cout << (std::chrono::duration<double>(end_t - start_t).count() / 60)
-            << " minutes" << std::endl;
+  print_duration(start_t, end_t);
 
   return 0;
 }
