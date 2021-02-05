f175982873e179c88bead6353b3997023eb78875
Xie Xie <DSAD2015@163.com>
2021-02-05 20:09:25 +0800

    Create iterative_Fibonacci.cpp

---
+++ loop_invariants/iterative_Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+uint64_t Fibonacci(size_t n)
+{
+  if (n < 2)
+    return n;
+  uint64_t x = 0;
+  uint64_t y = 1;
+  // 也可以只用n控制循环.
+  for (size_t i = 2; i < n; ++i)
+  {
+    uint64_t f = x + y;
+    x = y;
+    y = f;
+  }
+  return x + y;
+}
+
+int main()
+{
+  size_t n;
+  cin >> n;
+  cout << Fibonacci(n) << endl;
+  return 0;
+}
