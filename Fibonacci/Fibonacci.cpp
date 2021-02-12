64fac2fcc4b71d64fb3a53224c6c93b4c3b9ab40
Xie Xie <DSAD2015@163.com>
2021-02-12 23:15:14 +0800

    Update Fibonacci.cpp

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,13 +1,13 @@
-#include <utility>
+#include <tuple>
 
-std::pair<uint64_t, uint64_t> Fib(size_t n)
+std::tuple<uint64_t, uint64_t> Fib(size_t n)
 {
   // 返回F_{n}和F_{n + 1}, 注意是对UINT64_MAX取模的结果.
   if (n > 0)
   {
     auto PF = Fib(n / 2);   // 基于递归求解.
-    auto t0 = PF.first;
-    auto t1 = PF.second;
+    auto t0 = std::get<0>(PF);
+    auto t1 = std::get<1>(PF);
     if (n % 2 == 1)         // 奇数情况.
       return {t0 * t0 + t1 * t1, (2 * t0 + t1) * t1};
     else
