1a388149503fa8658259f93da7575d9f30ffae20
Xie Xie <DSAD2015@163.com>
2021-02-19 12:57:54 +0800

    Update search.cpp

--- string_matching/search.cpp
+++ string_matching/search.cpp
@@ -7,7 +7,7 @@ int main()
 {
   std::string p = "Algorithm_";
   std::string t = "Data Structures and Algorithms";
-	auto iter = std::search(t.begin(), t.end(), p.begin(), p.end());
+  auto iter = std::search(t.begin(), t.end(), p.begin(), p.end());
   std::cout << iter - t.begin() << std::endl;
   p = "Algorithms";
   iter = std::search(t.begin(), t.end(),
