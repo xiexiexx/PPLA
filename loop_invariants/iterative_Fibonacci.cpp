14ccfe7234fe95841e31445bae0a4e8041956b40
Xie Xie <DSAD2015@163.com>
2021-03-31 17:36:06 +0800

    Update iterative_Fibonacci.cpp

--- loop_invariants/iterative_Fibonacci.cpp
+++ loop_invariants/iterative_Fibonacci.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 uint64_t Fibonacci(size_t n)
 {
@@ -8,7 +10,7 @@ uint64_t Fibonacci(size_t n)
     return n;
   uint64_t x = 0;
   uint64_t y = 1;
-  // 也可以只用n控制循环.
+  // 也可以只用n控制循环, while (n-- > 2)即可.
   for (size_t i = 2; i < n; ++i)
   {
     uint64_t f = x + y;
